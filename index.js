408b493de843d468cecdd040fbeb703fe39c24c5
Sukhraj Singh Mann <manns20@coventry.ac.uk>
2024-03-11 14:51:57 +0000

    Database added With working GET PUT POST DELETE working with postman

--- index.js
+++ index.js
@@ -1,41 +1,13 @@
-// My blog API
-
-// Set up the application and its router
-
 const Koa = require('koa');
-const Router = require('koa-router');
-
 
 const app = new Koa();
-const router = new Router();
 
+const special = require('./routes/special.js')
+const books = require('./routes/books.js');
 
-/*
- * Define route handler(s):
- * 
- * This means we connect HTTP methods: GET, POST, ...
- * and URI paths: /some/uri/path
- * to JavaScript functions that handle the request.
- * 
- * Once defined we then add them to the app object.
-*/
-router.get('/api/v1', welcomeAPI);
-app.use(router.routes());
+app.use(special.routes());
+app.use(books.routes());
 
+let port = process.env.PORT || 3000;
 
-
-
-// Define the actual handler functions
-
-function welcomeAPI(ctx, next) {
-  ctx.body = {
-    message: "Welcome to the blog API!"
-  }
-}
-
-const articles = require('./routes/articles.js');
-app.use(articles.routes());
-
-// Finally, run the app as a process on a given port
-
-app.listen(3000);
+app.listen(port);
